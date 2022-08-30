44e47e95d6b6a40e052c0d5297f17bae3101edad
Matheus Muriel <matheus.muriel@outlook.com>
2022-08-29 22:26:52 -0300

    Audit Table

---
+++ clear.sql
@@ -0,0 +1,30 @@
+
+
+-- Drop all defined to restart environment
+
+-- Drop trigger
+DROP TRIGGER produto_config_update_trigger;
+
+-- Drop tables
+DROP TABLE person_obj_table;
+DROP TABLE apolice_obj_table;
+DROP TABLE produto_config_obj_table;
+DROP TABLE produto_config_auditoria;
+
+-- Drop varrays
+DROP TYPE item_array;
+
+-- Drop types
+
+DROP TYPE person_typ;
+DROP TYPE corretor_typ;
+DROP TYPE segurado_typ;
+
+DROP TYPE item_typ;
+DROP TYPE apolice_typ;
+DROP TYPE proposta_typ;
+DROP TYPE cotacao_typ;
+
+DROP TYPE produto_config_typ;
+
+
--- project.sql
+++ project.sql
@@ -1,5 +1,4 @@
 
---DROP TYPE person_typ;
 CREATE TYPE person_typ AS OBJECT (
   id        NUMBER,
   nome      VARCHAR2(20),
@@ -8,29 +7,23 @@ CREATE TYPE person_typ AS OBJECT (
   --MEMBER PROCEDURE display_details ( SELF IN OUT NOCOPY person_typ )
 ) NOT FINAL;
 
---DROP TYPE corretor_typ;
 CREATE TYPE corretor_typ UNDER person_typ (
   corretora   VARCHAR2(20)
 ) FINAL;
 
---DROP TYPE segurado_typ;
 CREATE TYPE segurado_typ UNDER person_typ (
   limite      NUMBER
 ) FINAL;
 
---DROP TABLE person_obj_table;
-CREATE TABLE person_obj_table OF person_typ;
+CREATE TABLE person_obj_table OF person_typ (
+  id PRIMARY KEY,
+  cpf UNIQUE
+);
 
 
-INSERT INTO person_obj_table VALUES (
-  person_typ(1,'Joao Silva',123)
-);
-INSERT INTO person_obj_table VALUES (
-  corretor_typ(2,'Gustavo Santos',234,'Corretora1')
-);
-INSERT INTO person_obj_table VALUES (
-  segurado_typ(3,'Augusto',345,1000)
-);
+INSERT INTO person_obj_table VALUES (person_typ(1,'Joao Silva',123));
+INSERT INTO person_obj_table VALUES (corretor_typ(2,'Gustavo Santos',234,'Corretora1'));
+INSERT INTO person_obj_table VALUES (segurado_typ(3,'Augusto',345,1000));
 
 SELECT VALUE(p) FROM person_obj_table p;
 
@@ -42,9 +35,13 @@ CREATE TYPE item_typ AS OBJECT (
   valor         NUMBER
 )
 
+CREATE TYPE item_array AS 
+  VARRAY(10) OF item_typ
+;
+
 CREATE TYPE apolice_typ AS OBJECT (
   id              NUMBER,
-  listaItens      VARRAY,
+  listaItens      item_array,
   valor           NUMBER,
   segurado        SEGURADO_TYP,
   corretor        CORRETOR_TYP,
@@ -60,3 +57,46 @@ CREATE TYPE proposta_typ UNDER apolice_typ (
 CREATE TYPE cotacao_typ UNDER proposta_typ (
   dataCotacao       DATE
 )
+
+CREATE TABLE apolice_obj_table OF apolice_typ;
+
+
+--- Tabela composta de Auditoria
+CREATE TYPE produto_config_typ AS OBJECT (
+  produto_id      NUMBER,
+  base_de_calculo NUMBER,
+  valor_minimo    NUMBER
+) FINAL;
+
+CREATE TABLE produto_config_obj_table OF produto_config_typ (
+  produto_id PRIMARY KEY
+);
+
+CREATE TABLE produto_config_auditoria (
+  data_alteracao    NUMBER,
+  config_produto    produto_config_typ
+);
+
+INSERT INTO produto_config_obj_table VALUES (produto_config_typ(1,2,3));
+INSERT INTO produto_config_auditoria VALUES (1, produto_config_typ(10,20,30));
+
+CREATE OR REPLACE TRIGGER produto_config_update_trigger
+AFTER UPDATE
+  ON produto_config_obj_table
+FOR EACH ROW
+DECLARE old_config produto_config_typ;
+BEGIN
+  INSERT INTO produto_config_auditoria 
+  VALUES (1, produto_config_typ(
+      :old.produto_id,
+      :old.base_de_calculo,
+      :old.valor_minimo
+      )
+  );
+END;
+
+UPDATE produto_config_obj_table p SET p.base_de_calculo=10 WHERE p.produto_id=1;
+SELECT * FROM produto_config_auditoria;
+
+
+
